97ea1dd1ced9c1f9654f056ebf35688e2c3c750d
Phil-gy <147167302+Phil-gy@users.noreply.github.com>
2026-05-26 20:21:43 +0200

    trying to fix scraping error

--- multi_language/recipe_collector/backend/app/main.py
+++ multi_language/recipe_collector/backend/app/main.py
@@ -21,7 +21,12 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://deine-recipe-app.vercel.app",
+
+        "https://www.rabeasrezepte.de",
+        "https://rabeasrezepte.de",
+
+        "http://www.rabeasrezepte.de",
+        "http://rabeasrezepte.de",
     ],
     allow_credentials=True,
     allow_methods=["*"],
